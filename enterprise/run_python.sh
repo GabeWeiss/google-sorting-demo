ea9510bf7e62d6c23846e87e21161e1988596596
Gabe Weiss <gweiss@google.com>
2019-03-06 23:48:50 +0000

    add streaming

--- enterprise/run_python.sh
+++ enterprise/run_python.sh
@@ -1,6 +1,6 @@
 nmcli dev wifi connect EdgeTPU24 ifname wlan0 password blacklotus
 
-python3 capture_and_recognize.py \
+python3 streaming_capture_and_recognize.py \
 	--model-file=edgetpu_model.tflite.2_27_2019 \
 	--video-device-index=1 \
 	--server-url=http://192.168.42.100:8080 \
