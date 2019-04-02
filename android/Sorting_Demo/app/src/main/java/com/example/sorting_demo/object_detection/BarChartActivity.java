30fc8edf25c19cbe90d85b78b0741ea111822a94
Gus Martins <lgusm@lgusm-macbookpro2.roam.corp.google.com>
2019-04-02 23:08:11 +0100

    First Barchart implementation

---
+++ android/Sorting_Demo/app/src/main/java/com/example/sorting_demo/object_detection/BarChartActivity.java
@@ -0,0 +1,17 @@
+package com.example.sorting_demo.object_detection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sorting_demo.R;
+
+public class BarChartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar_chart);
+
+
+    }
+}
