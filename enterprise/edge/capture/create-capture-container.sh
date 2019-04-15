ebb7f39545489902726d32d7d301733b4a70440b
Gabe Weiss <GabeWeiss@users.noreply.github.com>
2019-04-15 16:40:41 -0700

    Sadly, removing all reference to Edge in our project.

--- enterprise/edge/capture/create-capture-container.sh
+++
@@ -1,6 +0,0 @@
-gcloud alpha iot edge containers create capture_v1 \
-                                        --region us-central1 \
-                                        --registry edge_device_registry \
-                                        --device enterprise_board \
-                                        --docker-image gcr.io/sorting-demo-230918/capture:aarch64 \
-                                        --device-binding /dev/video1:/dev/video0
