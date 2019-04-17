da331ff9abe12422b96d290669508c9e38a1c60e
Gabe Weiss <GabeWeiss@users.noreply.github.com>
2019-04-17 10:48:21 -0700

    Adding a little test script to run to moc the enterprise board sending data.

---
+++ lenovo/enterprise_moc.py
@@ -0,0 +1,10 @@
+import requests
+
+d = {
+    'number':'01',
+    'confidence':0.765,
+    'inference_time':0.02965432
+}
+
+for x in range(10):
+    r = requests.post("http://localhost:8080", data = d)
