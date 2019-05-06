29ba4d037a3ae03ed0afe751307b8ea579637088
Yu-Han Liu <yuhanliu@google.com>
2019-05-06 08:00:29 -0700

    add models trained with new data

---
+++ enterprise/run_python_55.sh
@@ -0,0 +1,6 @@
+# nmcli dev wifi connect EdgeTPU24 ifname wlan0 password blacklotus
+
+python3 recognize.py \
+    --od-model-file models/model_od_bt_edgetpu.tflite.5_5_2019 \
+    --digit-model-file models/model_digit_bt_edgetpu.tflite.5_5_2019
+
